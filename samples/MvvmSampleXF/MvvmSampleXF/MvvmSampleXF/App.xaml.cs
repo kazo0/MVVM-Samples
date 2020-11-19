d190dab188d47ae40bb9909e442eec7e7e456a1d
Steve Bilogan <steve.bilogan@gmail.com>
2020-11-19 18:16:24 -0500

    Add sample for XF

---
+++ samples/MvvmSampleXF/MvvmSampleXF/MvvmSampleXF/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using MvvmSampleUwp.Services;
+using MvvmSampleXF.Services;
+using MvvmSampleXF.Views;
+using Refit;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MvvmSampleXF
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			// Register services
+			Ioc.Default.ConfigureServices(services =>
+			{
+				services.AddSingleton<IFilesService, FileService>();
+				services.AddSingleton<ISettingsService, SettingsService>();
+				services.AddSingleton(RestService.For<IRedditService>("https://www.reddit.com/"));
+			});
+			MainPage = new RelayCommandPage();
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
